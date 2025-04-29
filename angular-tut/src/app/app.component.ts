09a9d4b37b65d7b51c5bd47fb0b107abf4b4751e
Omkar Tarle <tarleomkar8@gmail.com>
2025-04-29 12:59:14 +0530

    Angular Data Binding With Signals

--- angular-tut/src/app/app.component.ts
+++ angular-tut/src/app/app.component.ts
@@ -1,39 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, SignupComponent, ProfileComponent],
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-
-export class AppComponent {
-  count = 0;
-
-  // handleIncrement() {
-  //   this.count = this.count + 1;
-  // }
-
-  // handleDecrement() {
-  //   this.count = this.count - 1;
-  // }
-
-  // handleReset() {
-  //   this.count = 0;
-  // }
-  
-  handleCounter(val: string) {
-    if(val == 'minus') {
-      this.count = this.count - 1;
-    } else if (val == 'plus') {
-      this.count = this.count + 1;
-    } else {
-      this.count = 0
-    }
-  }
-
-}
+export class AppComponent {}
---
+++ angular-tut/src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  title = signal('My First Angular app')
+}
