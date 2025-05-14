d2e7a112af60025e578f0a17fb6aa2169c95659a
Omkar Tarle <tarleomkar8@gmail.com>
2025-05-14 15:36:09 +0530

    control flow statement

--- angular-tut/src/app/app.component.ts
+++ angular-tut/src/app/app.component.ts
@@ -8,33 +8,18 @@ import { HeaderComponent } from './components/header/header.component';
   standalone: true,
   imports: [],
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles:['h2{background-color: pink}']
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  name="";
-  displayName = "";
-  email="";
+  display = true;
+  x = 20;
 
-  getName(event:Event) {
-    this.name = (event.target as HTMLInputElement).value
+  hide() {
+    this.display = false
   }
 
-  showName() {
-      this.displayName = this.name;
-    }
-
-    setName() {
-      this.name = "Sam"
-    }
-
-    getEmail(val:string) {
-      console.log(val);
-      this.email = val;
-    }
-
-    setEmail() {
-      this.email = "default@test.com"
-    }
+  show() {
+    this.display = true
   }
+}
